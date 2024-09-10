2f01df6a55b6960a7519c08bcad60c2e3be04a4e
Adnan-edu <md.ashikadnan@gmail.com>
2024-09-11 00:20:32 +1000

    Updated document metadata-Added pdf id

--- app/chat/create_embeddings.py
+++ app/chat/create_embeddings.py
@@ -24,6 +24,14 @@ def create_embeddings_for_pdf(pdf_id: str, pdf_path: str):
     )
     loader = PyPDFLoader(pdf_path)
     docs = loader.load_and_split(text_splitter)
+
+    #Update every documents metadata property
+    for doc in docs:
+        doc.metadata = {
+            "page": doc.metadata["page"],
+            "text": doc.page_content,
+            "pdf_id": pdf_id
+        }
     
     """
      * Vector store will use the embeddings object
