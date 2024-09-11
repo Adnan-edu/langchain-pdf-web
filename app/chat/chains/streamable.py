d7e1b7be9f8ad28b75f6576f93750f9b12a1b9f2
Adnan-edu <md.ashikadnan@gmail.com>
2024-09-12 02:09:05 +1000

    Integrating the Streaming Code in our project from the test file.

---
+++ app/chat/chains/streamable.py
@@ -0,0 +1,18 @@
+from queue import Queue
+from threading import Thread
+
+from app.chat.callbacks.stream import StreamingHandler
+
+
+class StreamableChain:
+    def stream(self, input):
+        queue = Queue()
+        handler = StreamingHandler(queue)
+        def task():
+            self(input, callbacks=[handler]) #Execute the Chain
+        Thread(target=task).start()
+        while True:
+            token = queue.get()
+            if token is None:
+                break
+            yield token
